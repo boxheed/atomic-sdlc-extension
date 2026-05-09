1e76e8707532a455452cb55169f5c30721d757dd
Andrew Dunn <boxheed@users.noreply.github.com>
2026-05-09 21:46:25 +0000

    fix: removed some extensions

--- .devcontainer/scripts/post-start/05-gemini-extensions.sh
+++ .devcontainer/scripts/post-start/05-gemini-extensions.sh
@@ -2,7 +2,7 @@
 set -e
 gemini extensions install https://github.com/github/github-mcp-server --consent || true
 gemini extensions install https://github.com/gemini-cli-extensions/conductor --consent || true
-gemini extensions install https://github.com/gemini-cli-extensions/nanobanana --consent || true
+#gemini extensions install https://github.com/gemini-cli-extensions/nanobanana --consent || true
 gemini extensions install https://github.com/gemini-cli-extensions/code-review --consent || true
 gemini extensions install https://github.com/gemini-cli-extensions/jules --consent || true
-gemini extensions install https://github.com/gemini-cli-extensions/ralph --consent || true
+#gemini extensions install https://github.com/gemini-cli-extensions/ralph --consent || true
